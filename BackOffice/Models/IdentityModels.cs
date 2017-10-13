7a3956f4323b3a6f9be49b24b8a0eb6763a3df22
Walkiria Suero <wsuero28@gmail.com>
2017-10-13 10:54:05 -0400

    Add project files.

---
+++ BackOffice/Models/IdentityModels.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BackOffice.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public virtual DbSet<Persona> Personas { get; set; }
+        public virtual DbSet<Telefono> Telefonos { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Persona>()
+                .ToTable("Personas")
+                .Property(x => x.Nombre)
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Telefono>()
+                .ToTable("Telefonos")
+                .Property(x => x.Numero)
+                .HasMaxLength(50);                
+                
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ BackOffice/Models/Telefono.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackOffice.Models
+{
+    public class Telefono
+    {
+        public int Id { get; set; }
+        public string Numero { get; set; }
+        public int PersonaId { get; set; }
+        public Persona Persona { get; set; }
+    }
+}
